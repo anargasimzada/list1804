f35b573eda0de735be4d0c29f2e428bba0abea91
anargasimzada <tu7g11auv@code.edu.az>
2024-04-18 02:44:59 +0400

    list

---
+++ List,DataStructureHW/Moduls/IntArrayList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_DataStructureHW.Moduls
+{
+    internal class IntArrayList
+    {
+        int[] _arr;
+        public int Capacity { get; set; } = 5;
+        public int Count { get; private set; } = 0;
+        public IntArrayList()
+        {
+            _arr = new int[Capacity];
+        }
+        public IntArrayList(int capacity)
+        {
+            Capacity = capacity;
+            _arr = new int[Capacity];
+        }
+        public void Add(int value)
+        {
+            if (_arr.Length == Count)
+            {
+                Array.Resize(ref _arr, Count + Capacity);
+            }
+            _arr[Count] = value;
+            Count++;
+        }
+        public void Remove(int value)
+        {
+
+        }
+        public void GetElements()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                Console.WriteLine(_arr[i]);
+            }
+        }
+        public int this[int i]
+        {
+            get { return _arr[i]; }
+            set { _arr[i] = value; }
+        }
+        public void IndexOf(int num)
+        {
+            for(int i = 0;i < _arr.Length;i++)
+            {
+                if(_arr[i] == num)
+                {
+                    Console.WriteLine(i);
+                }
+                else
+                {
+                    Count++;
+                }
+            }
+        }
+
+    }
+}
---
+++ List,DataStructureHW/Program.cs
@@ -0,0 +1,15 @@
+﻿using List_DataStructureHW.Moduls;
+
+namespace List_DataStructureHW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IntArrayList list = new IntArrayList();
+            list.Add(1);
+            list.GetElements();
+            list.IndexOf(5);
+        }
+    }
+}
